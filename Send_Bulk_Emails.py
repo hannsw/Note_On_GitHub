ccadfe6f7a20b106683ae0eae4fa9ed3e4c2a190
hannsw <33214874+hannsw@users.noreply.github.com>
2018-03-02 23:54:53 +1100

    Create Send_Bulk_Emails.py

---
+++ Send_Bulk_Emails.py
@@ -0,0 +1,33 @@
+import smtplib, sys, openpyxl
+
+#Excel
+wb = openpyxl.load_workbook('c:\\temp\\EmailList.xlsx')
+sheet = wb.get_sheet_by_name('Sheet1')
+
+#Populate Email Addresses into dictinonary
+EmailsToSend = {}
+for r in range(2, sheet.max_row -1):
+    name = sheet.cell(row=r, column=1).value
+    email = sheet.cell(row=r, column=2).value
+    PEN = sheet.cell(row=r, column=3).value
+    EmailsToSend[name] = {str(email):str(PEN)}
+
+#Send Emails
+smtpObj = smtplib.SMTP('smtp-mail.outlook.com', 587)
+smtpObj.ehlo()
+smtpObj.starttls()
+smtpObj.login('dhhh@live.com', 'Vi$ta2047')
+for name, subDict in EmailsToSend.items():
+    message = """
+    Subject: New PEN. \n
+    Dear %s,
+
+    Your new PEN is: %s
+
+    Regards,
+    ZR""" % (name, ''.join(list(subDict.values())))
+
+    print('Sending to %s ...' % name)
+    smtpObj.sendmail('dhhh@live.com',subDict.keys(), message)
+
+smtpObj.quit()
